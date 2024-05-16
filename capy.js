2ef0aaa11a8da6921a35403d93be06efdb3de65d
JETZPAD <eugenio.animali@proton.me>
2024-05-16 11:44:29 +0200

    changed scale

--- capy.js
+++ capy.js
@@ -7,8 +7,8 @@ let boardHeight = 250;
 let context;
 
 //capy
-let capyWidth = 90;
-let capyHeight = 90;
+let capyWidth = 70; // 90
+let capyHeight = 70;// 90
 let capyX = 50;
 let capyY = boardHeight - capyHeight;
 let capyImg;
@@ -24,11 +24,11 @@ let capy = {
 let bookArray = [];
 let book;
 
-let book1Width = 63;
-let book2Width = 69;
-let book3Width = 102;
+let book1Width = 45; //63
+let book2Width = 60; // 80
+let book3Width = 80; // 100
 
-let bookHeight = 70;
+let bookHeight = 50; // 79
 let bookX = 800;
 let bookY = boardHeight - bookHeight;
 
@@ -37,43 +37,41 @@ let book2Img;
 let book3Img;
 
 //physics
-let velocityX = -6; //book moving left speed
+let velocityX = -4.5; //6 book moving left speed
 let velocityY = 0;
-let gravity = .4;
+let gravity = .32; // .4
 
 let gameOver = false;
 let startagain = false;
 let score = 0;
 let highscore = 0;
 
-window.onload = startgame()
-
 function startgame() {
     board = document.getElementById("board");
     board.height = boardHeight;
     board.width = boardWidth;
-
+    
     context = board.getContext("2d"); //used for drawing on the board
-
+    
     //draw initial capysaur
     // context.fillStyle="green";
     // context.fillRect(capy.x, capy.y, capy.width, capy.height);
-
+    
     capyImg = new Image();
     capyImg.src = "./img/capy1.png";
     capyImg.onload = function() {
         context.drawImage(capyImg, capy.x, capy.y, capy.width, capy.height);
     }
-
+    
     book1Img = new Image();
     book1Img.src = "./img/book1.png";
-
+    
     book2Img = new Image();
     book2Img.src = "./img/book1.png";
-
+    
     book3Img = new Image();
     book3Img.src = "./img/book1.png";
-
+    
     requestAnimationFrame(update);
     setInterval(placebook, 1000); //1000 milliseconds = 1 second
     document.addEventListener("keydown", movecapy);
@@ -87,7 +85,7 @@ function update() {
             capyImg.onload = function() {
                 context.drawImage(capyImg, capy.x, capy.y, capy.width, capy.height);
             }
-            velocityX = -6;
+            velocityX = -4.5; // 6
             bookArray = []
             score = 0;
             gameOver = false;
@@ -97,20 +95,20 @@ function update() {
         }
     }
     context.clearRect(0, 0, board.width, board.height);
-
+    
     //capy
     velocityY += gravity;
     capy.y = Math.min(capy.y + velocityY, capyY); //apply gravity to current capy.y, making sure it doesn't exceed the ground
-
+    
     //book
     for (let i = 0; i < bookArray.length; i++) {
         let book = bookArray[i];
         book.x += velocityX;
         context.drawImage(book.img, book.x, book.y, book.width, book.height);
     }
-
+    
     detectCollision(score)
-
+    
     if (gameOver) {
         capyImg.src = "./img/capy-dead.png";
         capyImg.onload = function() {
@@ -119,10 +117,10 @@ function update() {
     } else {
         context.drawImage(capyImg, capy.x, capy.y, capy.width, capy.height);
     }
-
+    
     // speed up
-    velocityX -= 0.005
-
+    velocityX -= 0.003 // 5
+    
     //score
     context.fillStyle="black";
     context.font="20px courier";
@@ -135,22 +133,22 @@ function movecapy(e) {
     if (gameOver) {
         startagain = true;
     }
-
+    
     if ((e.code == "Space" || e.code == "ArrowUp") && capy.y == capyY) {
         //jump
-        velocityY = -10;
+        velocityY = -8; // 10
     }
     else if (e.code == "ArrowDown" && capy.y == capyY) {
         //duck
     }
-
+    
 }
 
 function placebook() {
     if (gameOver) {
         return;
     }
-
+    
     //place book
     let book = {
         img : null,
@@ -159,9 +157,9 @@ function placebook() {
         width : null,
         height: bookHeight
     }
-
+    
     let placebookChance = Math.random(); //0 - 0.9999...
-
+    
     if (placebookChance > .90) { //10% you get book3
         book.img = book3Img;
         book.width = book3Width;
@@ -177,13 +175,13 @@ function placebook() {
         book.width = book1Width;
         bookArray.push(book);
     }
-
+    
     if (bookArray.length > 5) {
         bookArray.shift(); //remove the first element from the array so that the array doesn't constantly grow
     }
 }
 
-function detectCollision(score) {
+function detectCollision() {
     for (let i = 0; i < bookArray.length; i++) {
         book = bookArray[i];
         if (capy.x < book.x + book.width &&   //a's top left corner doesn't reach b's top right corner
@@ -197,3 +195,5 @@ function detectCollision(score) {
         }
     }
 }
+
+window.onload = startgame()
